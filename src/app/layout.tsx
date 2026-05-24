16ae41f3195d40622815a32ad6330f39b8656d2f
Leenay97 <skynekop@gmail.com>
2026-05-24 10:47:07 +0800

    fix: comment hovers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={chiron.className}>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="preload" href="/bg-colored.png" as="image" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
