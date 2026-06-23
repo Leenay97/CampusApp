c92820d0ce580f7f64723b28f1c5f751e3e41697
Leenay97 <skynekop@gmail.com>
2026-06-24 00:08:48 +0800

    fix: same

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 'use client';
 import './globals.css';
-import { ApolloWrapper } from '@/lib/ApolloWrapper';
-import { UserProvider } from '@/contexts/UserContext';
-import { AppProvider } from '@/contexts/AppContext';
-import ModalLoading from '@/components/ModalLoading/ModalLoading';
-import { useLoading } from '@/contexts/LoadingContext';
-import { LoadingProvider } from '@/contexts/LoadingContext';
 import { roboto } from './fonts';
 
 export default function RootLayout({
@@ -13,33 +7,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  function ModalLoadingWrapper() {
-    const { loadingState, hideLoading } = useLoading();
-
-    if (!loadingState) return null;
-
-    return <ModalLoading loadingState={loadingState} onClose={hideLoading} />;
-  }
-
   return (
     <html lang="en" className={roboto.className}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="preload" href="/bg-colored.png" as="image" />
       </head>
-      <body className={`antialiased`}>
-        <ApolloWrapper>
-          <LoadingProvider>
-            <UserProvider>
-              <AppProvider>
-                {children}
-                <ModalLoadingWrapper />
-                <div id="modal-root" />
-              </AppProvider>
-            </UserProvider>
-          </LoadingProvider>
-        </ApolloWrapper>
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
