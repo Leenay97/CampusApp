0e751901b6742b4959d23be604a4036b0103a89f
Leenay97 <skynekop@gmail.com>
2026-07-23 05:58:55 +0800

    feat: add link to groupQR

--- src/app/(auth)/(teacher)/teacher/group/qr/page.tsx
+++ src/app/(auth)/(teacher)/teacher/group/qr/page.tsx
@@ -1,18 +1,42 @@
 'use client';
 
+import { useState } from 'react';
 import FullscreenContainer from '@/components/FullscreenContainer/FullscreenContainer';
 import Section from '@/components/Section/Section';
+import PrimaryButton from '@/components/PrimaryButton/PrimaryButton';
 import { useUser } from '@/contexts/UserContext';
 import { QRCodeCanvas } from 'qrcode.react';
+import styles from './GroupQrPage.module.scss';
 
 function TeacherGroupPage() {
   const { user } = useUser();
+  const [copied, setCopied] = useState(false);
 
   const groupUrl = `${window.location.origin}/register?token=${user?.group?.id}`;
 
+  async function handleCopy() {
+    await navigator.clipboard.writeText(groupUrl);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  }
+
   return (
     <FullscreenContainer>
-      <Section>{user?.group?.id && <QRCodeCanvas value={groupUrl} size={200} />}</Section>
+      <Section>
+        <div className={styles['group-qr']}>
+          {user?.group?.id && (
+            <>
+              <QRCodeCanvas value={groupUrl} size={200} />
+              <div className={styles['group-qr__link-row']}>
+                <div className={styles['group-qr__link']}>{groupUrl}</div>
+                <PrimaryButton onClick={handleCopy}>
+                  {copied ? 'Скопировано' : 'Скопировать'}
+                </PrimaryButton>
+              </div>
+            </>
+          )}
+        </div>
+      </Section>
     </FullscreenContainer>
   );
 }
